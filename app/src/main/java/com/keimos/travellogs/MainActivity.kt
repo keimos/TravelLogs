4831d200f9e886d06618d77cc5445397d9e5c59a
keimos <keithmosleyjr@gmail.com>
2021-07-03 02:13:02 -0400

    Completed working app

--- app/src/main/java/com/keimos/travellogs/MainActivity.kt
+++ app/src/main/java/com/keimos/travellogs/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.keimos.travellogs
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.lifecycle.Observer
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import androidx.recyclerview.widget.RecyclerView
 import com.keimos.travellogs.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,35 +22,26 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        // prep list view and RecyclerView (cells)
+        setupRecyclerView(item_list)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    // recycler view is the list of cells
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+
+        //update cell when note are modified
+        UserData.notes().observe(this, Observer<MutableList<UserData.Note>> { notes ->
+            Log.d(TAG, "Notes observer received ${notes.size} notes")
+
+            // let's create a RecyclerViewAdapter that manages the individual cells
+            recyclerView.adapter = NoteRecyclerNewAdapter(notes)
+
+        })
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
+
